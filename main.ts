9d4c537a6c9514f6f30628017ba40c87d0dd919f
richdartingfruit <76633464+richdartingfruit@users.noreply.github.com>
2020-12-27 15:03:35 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,8 +19,8 @@ input.onButtonPressed(Button.B, function () {
 let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 basic.forever(function () {
     if (input.lightLevel() < 10) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    } else if (input.lightLevel() > 15) {
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (input.lightLevel() > 15) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
 })
