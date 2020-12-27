55cf84694960f6eee654346a86a6e5ebeecb42b3
richdartingfruit <76633464+richdartingfruit@users.noreply.github.com>
2020-12-27 14:59:34 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,3 +16,11 @@ input.onButtonPressed(Button.B, function () {
         # # # # #
         `)
 })
+let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (input.lightLevel() < 10) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (input.lightLevel() > 15) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+})
